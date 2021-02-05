cd672b019fa743beec18f0a0f8894c1a1906dc17
Amin Adel <AminAdel@users.noreply.github.com>
2021-02-06 01:46:19 +0330

    Create config-dev.php

---
+++ app/config-dev.php
@@ -0,0 +1,18 @@
+<?php
+return [
+	
+	'env' => 'dev',
+	
+	'max_allowed_requests_per_second' => 5,
+	'max_allowed_warnings' => 3,
+	
+	"mysql" => [
+		"host" => 'localhost',
+		"db_name" => 'develo',
+		"username" => 'root',
+		"password" => ''
+	],
+	
+	'site' => 'sitename.xyz',
+	'path_public' => '/path-to/sitename.xyz/public_html/'
+];
