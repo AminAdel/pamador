6f236373dbbc165a064c8e209cf1425c22df5f74
Amin Adel <AminAdel@users.noreply.github.com>
2021-02-06 01:37:50 +0330

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,18 @@
 
+// security -> check ip black-list :
+$ip = $_SERVER['REMOTE_ADDR'];
+if ($_SERVER['HTTP_HOST'] == 'localhost') { $ip = '127.0.0.1'; }
+$ip = preg_replace("/[^0-9.]/", "", $ip);
+if(strlen($ip) < 5) exit('ip is not valid');
+if (file_exists('../ip_blacklist/' . $ip . '.txt')) {
+	exit('your ip has been blocked for unusual activity');
+}
+
+//==============================
+
+// write your redirects here :
+
+//==============================
+
+require '../app/vendor/autoload.php';
+include '../app/boot.php';
