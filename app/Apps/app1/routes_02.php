6dc4d4c8550feb29f377120dd5cff1cbc9eafd80
Amin Adel <AminAdel@users.noreply.github.com>
2024-06-12 09:09:36 +0330

    Update routes_02.php

--- app/Apps/app1/routes_02.php
+++ app/Apps/app1/routes_02.php
@@ -1,7 +1,7 @@
 <?php
 
 use Methods\Route;
-use Apps\App1\Controllers\StaticPagesController;
+use Apps\app1\Controllers\StaticPagesController;
 
 Route::get('/', StaticPagesController::class, 'home');
 Route::get('/about', StaticPagesController::class, 'about');
