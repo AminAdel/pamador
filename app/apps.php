47745b019af8e267d33f5f47ca11f2f976670bf5
Amin Adel <AminAdel@users.noreply.github.com>
2024-06-12 09:02:02 +0330

    Update apps.php

--- app/apps.php
+++ app/apps.php
@@ -6,26 +6,27 @@ switch ($GLOBALS['req_info']['server_name']) {
 		$GLOBALS['app'] = 'example_com';
 		break;
 	default :
-		$GLOBALS['app'] = 'aprofile_website';
+		$GLOBALS['app'] = 'app1';
 }
 
 //==============================
 
 // for each url parameter you can specify different app;
 // if set -> use array_shift to remove first parameter;
+// todo: there is some bug reported here related to params[0], find and fix it.
 switch ($GLOBALS['req_info']['params'][0] ?? '') {
-	// case 'aprofile_website':
-	// 	$GLOBALS['app'] = 'aprofile_website';
+	// case 'app1':
+	// 	$GLOBALS['app'] = 'app1';
 	// 	array_shift($GLOBALS['req_info']['params']);
 	// 	break;
 	default :
-		$GLOBALS['app'] = 'aprofile_website';
+		$GLOBALS['app'] = 'app1';
 }
 //==============================
 
 // you can write your logic to switch between apps :
 // if ($condition) {
-// 	$app = 'app';
+// 	$GLOBALS['app'] = 'app1';
 // }
 
 //==============================
